0bd184b00b35db8d85b2a8353dd2577c3f60d2e7
kslu <kengshil@usc.edu>
2017-01-06 03:03:14 +0800

    Add files via upload

--- hmat.m
+++ hmat.m
@@ -1,26 +1,35 @@
-function [H,H1]=hmat(gridsize,symtype)
-% [H,H1]=hmat(gridsize,symtype)
+function [H, H1]=hmat(gridsize, symtype)
+% [H, H1]=hmat(gridsize, symtype)
 %    return the butterfly-related matrix H corresponding to a given grid 
-%    size and a given symmetry type
+%    size and a given grid symmetry type
 % 
-% gridsize: grid size. For ud/lr/udlr/centro, gridsize=[height, width] 
-%    For other symmetry types, it is required that height=width. If this 
-%    input is a scalar N, it is regarded as a square grid, with 
-%    N=height=width. In the current version, both width and height have 
-%    to be even.
-% 
-% H: orthogonal matrix H (scaled version of the butterfly)
+% gridsize: grid size. For ud/lr/udlr/centro symmetry, 
+%    gridsize=[height, width]. For other symmetry types, it is required 
+%    that height=width. If this input is a scalar N, it is regarded as a 
+%    square grid with N=height=width. In the current version, both width 
+%    and height have to be even.
 %
-% H1: butterfly matrix H, with elements 0,1,-1
-%
-% symtype: grid symmetry type: 'ud', 'lr', 'udlr', 'centro', 'ds', 
-%    'as', 'bs', 'penta'
+% symtype: grid symmetry type, which can be 
+%    'ud' for UD-symmetry 
+%    'lr' for LR-symmetry
+%    'udlr' for UDLR-symmetry 
+%    'centro' for centrosymmetry
+%    'ds' for diagonal symmetry
+%    'as' for anti-diagonal symmetry 
+%    'bs' for bidiagonal symmetry 
+%    'penta' for pentasymmetry
 % 
+% H: orthogonal matrix H, as in [1]. It is a scaled version of the 
+%    butterfly
+%
+% H1: butterfly matrix H, whose elements are all 0,1,-1
+%
 % [1] K.-S. Lu and A. Ortega, "Fast Implementations for Symmetric Non-
 %    Separable Transforms Based on Grids," ICASSP 2017.
 % 
 % by: KS Lu
-% last update: 20170104
+% last update: 20170105
+
 
 % handle grid height and width
 if numel(gridsize)==1
@@ -50,7 +59,7 @@ matI = reshape(1:NN,h,w);
 matI_half = reshape(1:NN/2,w,h/2);  % for recovering the order after H_ud
 lB=(NN-2*h)/4;  % length of key sub-blocks
 
-% prepare permutation matrices
+% prepare node reordering permutation matrices
 switch symtype
     case {'ud', 'udlr'}
         
@@ -72,6 +81,8 @@ switch symtype
         idx_diags(h/2+1:end,:)=idx_diags(h/2+1:end,[2,1]);
         matI_0diag=matI; 
         matI_0diag(1:h+1:end)=0; matI_0diag(h:h-1:end)=0;
+        % matI_0diag contains the labels 1 to N^2 in a column-first 
+        % manner, but with both diagonals removed
         matI_u=triu(fliplr(triu(fliplr(matI_0diag))));
         idx_u=rot90(matI_u,2);
         idx_u(:,1:2:end)=flipud(idx_u(:,1:2:end));
@@ -88,8 +99,8 @@ switch symtype
         idx_r=fliplr(matI_r');
         idx_r(:,1:2:end)=flipud(idx_r(:,1:2:end));
         idx_r=idx_r(:)'; idx_r(idx_r==0)=[];
-        idx_db=[idx_diags(:)', idx_l, idx_d, idx_r, idx_u];
-        P_db = I_NN(:,idx_db);
+        idx_bs=[idx_diags(:)', idx_l, idx_d, idx_r, idx_u];
+        P_bs = I_NN(:,idx_bs);
         
 end
 
@@ -118,49 +129,49 @@ switch symtype
         % should be corrected later
         
     case 'ds'
-        idx_d1 = zeros(1,NN);
-        idx_d1(1:h)=diag(matI,0)';
+        idx_ds = zeros(1,NN);
+        idx_ds(1:h)=diag(matI,0)';
         curI=h;
         for i=[-h+1:-1, 1:h-1]
             idx_in_zigzag = curI+(1:(h-abs(i)));
             if (i<0 && mod(i,2)==0) || (i>0 && mod(i,2)==1)  % reverse order
-                idx_d1(fliplr(idx_in_zigzag)) = diag(matI,i)';
+                idx_ds(fliplr(idx_in_zigzag)) = diag(matI,i)';
             else
-                idx_d1(idx_in_zigzag) = diag(matI,i)';
+                idx_ds(idx_in_zigzag) = diag(matI,i)';
             end
             curI=curI+h-abs(i);
         end
-        P_d1 = I_NN(:,idx_d1);
+        P_ds = I_NN(:,idx_ds);
         %P_alpha=I_NN(:,[1:h, h*(h+1)/2+1:NN, h+1:h+h*(h-1)/2]);  % large block first
         P_alpha=I_NN(:,[h+1:h+h*(h-1)/2, 1:h, h*(h+1)/2+1:NN]); % small block first
-        H1=P_d1*blkdiag(eye(h),kmat1(NN-h)')*P_alpha;
+        H1=P_ds*blkdiag(eye(h),kmat1(NN-h)')*P_alpha;
     case 'as'
-        idx_d2 = zeros(1,NN);
-        idx_d2(1:h)=diag(fliplr(matI),0)';
+        idx_as = zeros(1,NN);
+        idx_as(1:h)=diag(fliplr(matI),0)';
         curI=h;
         for i=[-h+1:-1, 1:h-1]
             idx_in_zigzag = curI+(1:(h-abs(i)));
             if (i<0 && mod(i,2)==0) || (i>0 && mod(i,2)==1) 
-                idx_d2(fliplr(idx_in_zigzag)) = diag(fliplr(matI),i)';
+                idx_as(fliplr(idx_in_zigzag)) = diag(fliplr(matI),i)';
             else
-                idx_d2(idx_in_zigzag) = diag(fliplr(matI),i)';
+                idx_as(idx_in_zigzag) = diag(fliplr(matI),i)';
             end
             curI=curI+h-abs(i);
         end
-        P_d2 = I_NN(:,idx_d2);
+        P_as = I_NN(:,idx_as);
         %P_alpha=I_NN(:,[1:h, h*(h+1)/2+1:NN, h+1:h+h*(h-1)/2]);  % large block first
         P_alpha=I_NN(:,[h+1:h+h*(h-1)/2, 1:h, h*(h+1)/2+1:NN]); % small block first
-        H1=P_d2*blkdiag(eye(h),kmat1(NN-h)')*P_alpha;
+        H1=P_as*blkdiag(eye(h),kmat1(NN-h)')*P_alpha;
     case 'bs'
         P_beta=I_NN(:,[2*h+1:2*h+lB, h/2+1:h, 2*h+lB+1:2*h+2*lB, 1:h/2, ...
             2*h+2*lB+1:2*h+3*lB, h+1:2*h, 2*h+3*lB+1:NN]);
-        H1=P_db*blkdiag(kmat1(2*h)',kmat1(NN-2*h)')...
+        H1=P_bs*blkdiag(kmat1(2*h)',kmat1(NN-2*h)')...
             *blkdiag(eye(2*h),kron(eye(2),kmat1(NN/2-h))')*P_beta;
     case 'penta'
         P_gamma=I_NN(:,[2*h+1:2*h+lB, h/2+1:h, 2*h+lB+1:2*h+2*lB, 1:h/2, ...
             2*h+2*lB+1:2*h+3*lB, h+1:h+h/2, 2*h+3*lB+1:2*h+3*lB+lB/2, ...
             h+h/2+1:2*h, 2*h+3*lB+lB/2+1:NN]);
-        H1=P_db*blkdiag(kmat1(2*h)',kmat1(NN-2*h)')...
+        H1=P_bs*blkdiag(kmat1(2*h)',kmat1(NN-2*h)')...
             *blkdiag(eye(2*h),kron(eye(2),kmat1(NN/2-h)'))...
             *blkdiag(eye(h),kmat1(h)',kmat1(NN/4-h/2)',eye(NN/2-h),...
             kmat1(NN/4-h/2)')*P_gamma;
